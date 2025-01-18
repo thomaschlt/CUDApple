9bedba0f98f31f2dd0743510f4b56af86b400961
Thomas Chimbault <tchimbault@ensc.fr>
2025-01-19 00:35:39 +0100

    adding cuda examples to support

---
+++ src/examples/softmax/naive_0.cu
@@ -0,0 +1,29 @@
+__global__ void softmax_kernel_0(float *matd, float *resd, int M, int N)
+{
+    int row = blockDim.x * blockIdx.x + threadIdx.x;
+
+    if (row < M)
+    {
+        // max
+        float m = -1 * INFINITY;
+        // norm factor
+        float L = 0.0f;
+
+        // 3 passes (not optimal)
+        for (int col = 0; col < N; col++)
+        {
+            int i = row * N + col;
+            m = max(m, matd[i]);
+        }
+        for (int col = 0; col < N; col++)
+        {
+            int i = row * N + col;
+            L += expf(matd[i] - m);
+        }
+        for (int col = 0; col < N; col++)
+        {
+            int i = row * N + col;
+            resd[i] = expf(matd[i] - m) / L;
+        }
+    }
+}
--- src/examples/vector_add.cu
+++ src/examples/vector_add.cu
@@ -1,6 +1,9 @@
-__global__ void vectorAdd(float *a, float *b, float *c, int n) {
+// Kernel for vector addition
+__global__ void vectorAdd(float *vec1, float *vec2, float *res, int dim)
+{
     int i = blockIdx.x * blockDim.x + threadIdx.x;
-    if (i < n) {
-        c[i] = a[i] + b[i];
+    if (i < dim)
+    {
+        res[i] = vec1[i] + vec2[i];
     }
 }
